6fad827a427a2b5dd0155249078cd26265f757b0
amir birhanu <134446121+amir1a@users.noreply.github.com>
2023-07-08 17:13:12 +0000

    amir

---
+++ coverage/test.java
@@ -0,0 +1,13 @@
+package coverage;
+
+public class test{
+   public static void main(String[] args) {
+        System.out.println("HHHELLO");
+        for(int i=0;i<=5;i++){ 
+            for(int j=0;j<=i;j++){
+            System.out.println("*");
+               } 
+       }
+    
+            }
+}
